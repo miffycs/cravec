fd5edc54aefcbee33d8caebb288aa592d4eca311
Miffy Chen <mchen15@bu.edu>
2021-03-15 04:15:58 -0400

    Add everything

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from "./scrollToTop";
+
+// css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
+import '../node_modules/magnific-popup/dist/magnific-popup.css';
+import '../node_modules/react-perfect-scrollbar/dist/css/styles.css';
+import '../node_modules/leaflet/dist/leaflet.css';
+import './assets/fonts/flaticon/flaticon.css';
+import './assets/fonts/font-awesome/css/all.min.css';
+import './assets/css/style.css';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <ScrollToTop />
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
