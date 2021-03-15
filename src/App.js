3f2a6580cba17e93035994e9ad30df20b9bee701
Miffy Chen <mchen15@bu.edu>
2021-03-15 05:01:52 -0400

    use import instead of React.lazy() & Suspense, use Switch. Take out Preloader.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React, { Suspense, useLayoutEffect } from 'react'
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
-
-// Preloader
-const Preloader = React.lazy(() => import("./components/layouts/Preloader"));
+import React, { useLayoutEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 // Pages
-const Home = React.lazy(() => import("./components/pages/Home"));
-const Hometwo = React.lazy(() => import("./components/pages/Hometwo"));
-const Homethree = React.lazy(() => import("./components/pages/Homethree"));
-const Homefour = React.lazy(() => import("./components/pages/Homefour"));
-const Bloggrid = React.lazy(() => import("./components/pages/Bloggrid"));
-const Bloglist = React.lazy(() => import("./components/pages/Bloglist"));
-const Blogmasonry = React.lazy(() => import("./components/pages/Blogmasonry"));
-const Blogfull = React.lazy(() => import("./components/pages/Blogfull"));
-const Blogsingle = React.lazy(() => import("./components/pages/Blogsingle"));
-const About = React.lazy(() => import("./components/pages/About"));
-const Login = React.lazy(() => import("./components/pages/Login"));
-const Register = React.lazy(() => import("./components/pages/Register"));
-const Checkout = React.lazy(() => import("./components/pages/Checkout"));
-const Cart = React.lazy(() => import("./components/pages/Cart"));
-const Legal = React.lazy(() => import("./components/pages/Legal"));
-const Error = React.lazy(() => import("./components/pages/Error"));
-const Menuone = React.lazy(() => import("./components/pages/Menuone"));
-const Menutwo = React.lazy(() => import("./components/pages/Menutwo"));
-const Menuitemone = React.lazy(() => import("./components/pages/Menuitemone"));
-const Menuitemtwo = React.lazy(() => import("./components/pages/Menuitemtwo"));
-const Locations = React.lazy(() => import("./components/pages/Locations"));
-const Contact = React.lazy(() => import("./components/pages/Contact"));
+import Home from './components/pages/Home';
+import Hometwo from './components/pages/Hometwo';
+import Homethree from './components/pages/Homethree';
+import Homefour from './components/pages/Homefour';
+import Bloggrid from './components/pages/Bloggrid';
+import Bloglist from './components/pages/Bloglist';
+import Blogmasonry from './components/pages/Blogmasonry';
+import Blogfull from './components/pages/Blogfull';
+import Blogsingle from './components/pages/Blogsingle';
+import About from './components/pages/About';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import Checkout from './components/pages/Checkout';
+import Cart from './components/pages/Cart';
+import Legal from './components/pages/Legal';
+import Error from './components/pages/Error';
+import Menuone from './components/pages/Menuone';
+import Menutwo from './components/pages/Menutwo';
+import Menuitemone from './components/pages/Menuitemone';
+import Menuitemtwo from './components/pages/Menuitemtwo';
+import Locations from './components/pages/Locations';
+import Contact from './components/pages/Contact';
 
 const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
   useLayoutEffect(() => {
@@ -39,9 +36,8 @@ const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Suspense fallback</div>}>
-        <ScrollToTop>
-          <Preloader />
+      <ScrollToTop>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/home-v2" component={Hometwo} />
           <Route path="/home-v3" component={Homethree} />
@@ -65,8 +61,8 @@ function App() {
           <Route path="/menu-item-v2/:id" exact component={props => (<Menuitemtwo {...props} key={window.location.pathname} />)} />
           <Route path="/locations" component={Locations} />
           <Route path="/contact" component={Contact} />
-        </ScrollToTop>
-      </Suspense>
+        </Switch>
+      </ScrollToTop>
     </Router>
   );
 }
