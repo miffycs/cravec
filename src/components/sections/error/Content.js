5866940861477e35231dfd750773e68f8ca2210b
Miffy Chen <mchen15@bu.edu>
2021-03-19 20:58:25 -0400

    Error Page - bday edition

--- src/components/sections/error/Content.js
+++ src/components/sections/error/Content.js
@@ -7,8 +7,8 @@ class Content extends Component {
             <div className="page-404-wrapper">
                 <div className="page-404-text">
                     <h1>Page Not Found</h1>
-                    <p>How'd you get here?</p> <br />
-                    <Link to="/" className="btn-custom">Back to Home</Link>
+                    <p>How'd you get here? This is supposed to be a top-secret project... Well, at least till 3/24.</p> <br />
+                    <Link to="/" className="btn-custom">But this is my bday present</Link>
                 </div>
                 <div className="banner-bottom-img">
                     <img src={process.env.PUBLIC_URL + "/assets/img/veg/2.png"} alt="veg" />
